11724c011387ed37c1353788986b81b8f0e15291
Muhammed Kamal <muhammedkamal98@gmail.com>
2017-04-21 02:15:28 +0000

    first conflict solve

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  	before_action :logged_in_user, only: [:edit, :update]
+	before_action :current_user, only: [:edit, :update]
+
+  
   	def show
     	@user = User.find(params[:id])
 	end
@@ -7,6 +11,7 @@ class UsersController < ApplicationController
 	def new
     	@user = User.new
   	end
+
 
   	def create
     	@user = User.new(user_params)
@@ -24,11 +29,33 @@ class UsersController < ApplicationController
   		@user.save
   		redirect_to users
   	end
+  	def edit
+  	end
+  	def update
+  		if @user.update_attributes(user_params)
+  			flash[:success] = "Profile update"
+  			redirect_to @user
+  		else
+  			render 'edit'
+  		end
+  	end
   	
   	
   	private
     
 	    def user_params
 	      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	    end
+	    # before filters 
+	    #confirm a logged in user
+	    def logged_in_user
+	    	unless logged_in?
+	    	flash[:danger] = "Please log in."
+	    	redirect_to login_url 
+	    end
+	    end
+	    def correct_user
+	    	@user = User.find(params[:id])
+	    	redirect_to(root_url) unless @user = current_user?(@user)
 	    end
 end
