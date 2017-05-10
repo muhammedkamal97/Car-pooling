1d26f8fc62b0b27ec302062fae256bed39d14d39
Your Name <muhammedkamal98@gmail.com>
2017-05-10 15:22:10 +0300

    fixing some error

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -43,6 +43,7 @@ module SessionsHelper
 	end
 
 	def distance (loc1, loc2)
+		if(loc1 && loc2)
 	  		rad_per_deg = Math::PI/180  # PI / 180
 	  		rkm = 6371                  # Earth radius in kilometers
 	  		rm = rkm * 1000             # Radius in meters
@@ -62,5 +63,6 @@ module SessionsHelper
 		    a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
 		    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
 		    rm * c # Delta in meters
+		end
 	end	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504111209) do
+ActiveRecord::Schema.define(version: 20170510120618) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "day"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170504111209) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.string   "gender"
+    t.string   "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
